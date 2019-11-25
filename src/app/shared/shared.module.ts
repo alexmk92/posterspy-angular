0c9e0541e77404e1af2ae30789d0ba4150fbd45e
Alex Sims <alexander.sims92@gmail.com>
2019-11-25 22:29:12 +0000

    added modules

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+const components = [];
+
+const modules = [
+  RouterModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule,
+  LayoutModule,
+  MatSidenavModule,
+  MatListModule,
+  MatMenuModule,
+  MatIconModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSnackBarModule,
+];
+
+@NgModule({
+  declarations: [...components],
+  imports: [...modules],
+  exports: [
+      ...components,
+      ...modules
+  ]
+})
+export class SharedModule { }
