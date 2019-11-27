d8bb26cb08e4d860223b813990d186d1cdf38f3e
Alex Sims <alexander.sims92@gmail.com>
2019-11-27 20:49:59 +0000

    Added snackbar for showing auth errors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomePageComponent} from './home-page/home-page.component';
+import {AuthGuard} from './user/auth/auth.guard';
 
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent },
+  { path: '', component: HomePageComponent, canActivate: [AuthGuard] },
   {
     // Lazy load the route so we can reduce module bundle size
     path: 'login', loadChildren: () => import('./user/user.module').then(module => module.UserModule)
